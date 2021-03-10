6ee16f68538936275619b5af32d66ae4cbab138c
Killian <thevenin.killian@gmail.com>
2021-03-10 18:25:07 +0100

    edit translate

--- js/btn.js
+++ js/btn.js
@@ -1,57 +1,45 @@
-const btnPaused = document.getElementById('btn-startPaused')
-const btnStop = document.getElementById('btn-stop')
-const btnStart = document.getElementById('btn-startTimer')
-const time = document.getElementById('time')
+const btnPaused = document.getElementById('btn-startPaused');
+const btnStop = document.getElementById('btn-stop');
+const btnStart = document.getElementById('btn-startTimer');
+const time = document.getElementById('time');
 
 if (btnPaused !== null) {
-  let parsePause = parseInt(localStorage.getItem('pause'))
+  let parsePause = parseInt(localStorage.getItem('pause'));
 
-  if(parsePause !== 0){
-    time.textContent = localStorage.getItem('pause') + ' minutes'
+  if (parsePause !== 0) {
+    time.textContent = localStorage.getItem('pause') + ' minutes';
 
     btnPaused.addEventListener('click', () => {
-      localStorage.setItem('isPause', 'activePause')
-    })
-  }else {
-    time.textContent = 'Pas de pause !'
+      localStorage.setItem('isPause', 'activePause');
+    });
+  } else {
+    time.textContent = 'No pause !';
 
-    btnPaused.style.display = 'none'
+    btnPaused.style.display = 'none';
   }
 }
 
 if (btnStart !== null) {
-
-  let parseHour = parseInt(localStorage.getItem('hour'))
-  let parseMinute = parseInt(localStorage.getItem('minute'))
+  let parseHour = parseInt(localStorage.getItem('hour'));
+  let parseMinute = parseInt(localStorage.getItem('minute'));
 
   if (parseHour > 0 && parseMinute > 0) {
-
-    time.textContent = parseHour + 'h' + parseMinute + 'min'
-
+    time.textContent = parseHour + 'h' + parseMinute + 'min';
   } else if (parseHour === 0 && parseMinute > 0) {
-
-    time.textContent = parseMinute  + ' minutes'
-
+    time.textContent = parseMinute + ' minutes';
   } else if (parseHour > 1 && parseMinute === 0) {
-
-    time.textContent = parseHour + ' heures'
-
+    time.textContent = parseHour + ' hours';
   } else if (parseHour === 1 && parseMinute === 0) {
-
-    time.textContent = parseHour + ' heure'
-
+    time.textContent = parseHour + ' hour';
   } else {
-
-    time.textContent = '????'
-
+    time.textContent = '????';
   }
 
   btnStart.addEventListener('click', () => {
-    localStorage.setItem('isActive', 'activeWork')
-  })
-
+    localStorage.setItem('isActive', 'activeWork');
+  });
 }
 
 btnStop.addEventListener('click', () => {
-  localStorage.setItem('isStop', 'activeStop')
-})
+  localStorage.setItem('isStop', 'activeStop');
+});
--- js/options.js
+++ js/options.js
@@ -1,79 +1,86 @@
-const htmlBody = document.body
-const inputHour = document.getElementById('hour')
-const inputMinute = document.getElementById('minute')
-const inputPause = document.getElementById('pause')
-const tips = document.getElementById('tips')
+const htmlBody = document.body;
+const inputHour = document.getElementById('hour');
+const inputMinute = document.getElementById('minute');
+const inputPause = document.getElementById('pause');
+const tips = document.getElementById('tips');
 
-inputHour.value = localStorage.getItem('hour')
-inputMinute.value = localStorage.getItem('minute')
-inputPause.value = localStorage.getItem('pause')
+inputHour.value = localStorage.getItem('hour');
+inputMinute.value = localStorage.getItem('minute');
+inputPause.value = localStorage.getItem('pause');
 
 inputHour.addEventListener('input', (e) => {
-  addValue(e, 0, 24, 'hour', 0)
-})
+  addValue(e, 0, 24, 'hour', 0);
+});
 
 inputMinute.addEventListener('input', (e) => {
-  addValue(e, 0, 59, 'minute', 25)
-})
+  addValue(e, 0, 59, 'minute', 25);
+});
 
 inputPause.addEventListener('input', (e) => {
-  addValue(e, 0, 2880, 'pause', 5)
-})
+  addValue(e, 0, 2880, 'pause', 5);
+});
 
-focusEvent(inputHour, `Définir l'heure du timer avec un nombre compris entre 0 et 24`, 0)
-focusEvent(inputMinute, `Définir les minutes du timer avec un nombre compris entre 0 et 59`, 25)
-focusEvent(inputPause, `Définir le temps de pause avec un nombre (en minutes) compris entre 0 et 2880. Si valeur = 0, arrête le timer automatiquement`, 5)
+focusEvent(
+  inputHour,
+  `Set the hour of the timer with a number between 0 and 24`,
+  0
+);
+focusEvent(
+  inputMinute,
+  `Set the minute of the timer with a number between 0 and 59`,
+  25
+);
+focusEvent(
+  inputPause,
+  `Set the time of pause (in minute) between 0 and 2880. If set to 0, stop the timer automatically`,
+  5
+);
 
 function addValue(e, min, max, time, defaut) {
-
   if (e.target.value >= min && e.target.value <= max && e.target.value !== '') {
-    localStorage.setItem(time, e.target.value)
-    localStorage.setItem('change', true)
+    localStorage.setItem(time, e.target.value);
+    localStorage.setItem('change', true);
   }
 
   e.target.addEventListener('blur', () => {
     if (e.target.value === '') {
-      localStorage.setItem(time, defaut)
-      localStorage.setItem('change', true)
-      e.target.value = defaut
+      localStorage.setItem(time, defaut);
+      localStorage.setItem('change', true);
+      e.target.value = defaut;
     }
-  })
-
+  });
 }
 
 function focusEvent(input, text, defaut) {
-
   input.addEventListener('focusin', () => {
-    tips.innerHTML = text
-  })
+    tips.innerHTML = text;
+  });
 
   input.addEventListener('focusout', () => {
-    tips.innerHTML = ''
+    tips.innerHTML = '';
 
     if (input === inputHour) {
       if (input.value > 24) {
-        input.value = defaut
-        tips.innerHTML = 'error'
-        localStorage.setItem('hour', defaut)
+        input.value = defaut;
+        tips.innerHTML = 'error';
+        localStorage.setItem('hour', defaut);
       }
     }
 
     if (input === inputMinute) {
       if (input.value > 59) {
-        input.value = defaut
-        tips.innerHTML = 'error'
-        localStorage.setItem('minute', defaut)
+        input.value = defaut;
+        tips.innerHTML = 'error';
+        localStorage.setItem('minute', defaut);
       }
     }
 
     if (input === inputPause) {
       if (input.value > 2880) {
-        input.value = defaut
-        tips.innerHTML = 'error'
-        localStorage.setItem('pause', defaut)
+        input.value = defaut;
+        tips.innerHTML = 'error';
+        localStorage.setItem('pause', defaut);
       }
     }
-
-  })
-
+  });
 }
